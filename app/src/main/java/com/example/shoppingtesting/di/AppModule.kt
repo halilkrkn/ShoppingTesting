323958e37cfb90912edff4c65bdb48718141098d
halilkrkn <42476890+halilkrkn@users.noreply.github.com>
2022-09-17 13:58:06 +0300

    Retrofit and Dagger-Hilt Setup

---
+++ app/src/main/java/com/example/shoppingtesting/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.example.shoppingtesting.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.shoppingtesting.data.local.ShoppingItemDatabase
+import com.example.shoppingtesting.data.remote.PixabayAPI
+import com.example.shoppingtesting.other.Constants.BASE_URL
+import com.example.shoppingtesting.other.Constants.DATABASE_NAME
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context,
+    ) = Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providePixabayApi(): PixabayAPI {
+        return Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build().create(PixabayAPI::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/example/shoppingtesting/other/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.shoppingtesting.other
+
+object Constants {
+
+    const val DATABASE_NAME = "shopping_db"
+    const val BASE_URL = "https://pixabay.com"
+}
